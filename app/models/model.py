e2e8cc7203c93d46102c168544e71bdf39d34096
Sedrowow <sedrickfanderl8@gmail.com>
2024-09-16 12:00:45 +0200

    Refactor Model class to remove unused API key parameter

--- app/models/model.py
+++ app/models/model.py
@@ -3,14 +3,11 @@ from typing import Any
 
 
 class Model:
-    def __init__(self, model_name, base_url, api_key, context):
+    def __init__(self, model_name, base_url, context):
         self.model_name = model_name
         self.base_url = base_url
-        self.api_key = api_key
         self.context = context
 
-        if api_key:
-            os.environ['OPENAI_API_KEY'] = api_key
 
     def get_instructions_for_objective(self, *args) -> dict[str, Any]:
         pass
